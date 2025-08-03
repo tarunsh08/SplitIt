c6d4ec0ebf19340e1f22996d0a60e5368c213706
Tarun Sharma <tarunsharma08.com@gmail.com>
2025-08-04 00:37:26 +0530

    bug fixes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,23 +7,36 @@ import { useNavigate } from "react-router-dom";
 import { PointerHighlight } from "../components/ui/pointer-highlight";
 
 export function Home() {
-    const navigate = useNavigate()
+  const navigate = useNavigate()
+
+  const handleGetStarted = () => {
+    const isLoggedIn = localStorage.getItem('token');
+    if (!isLoggedIn) {
+      navigate("/login")
+    } else {
+      navigate("/splitit")
+    }
+  }
+
   return (
     <>
-    <div
-      className="min-h-screen relative w-full overflow-hidden dark:bg-slate-900 flex flex-col gap-10 items-center justify-center">
-      <div
-        className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
-      <Boxes />
-      <h1 className={cn("md:text-4xl text-xl font-semibold dark:text-white text-black relative z-20")}>
-        <PointerHighlight>SPLIT EXPENSES WITH FRIENDS</PointerHighlight>
-      </h1>
-      <p className="text-center mt-2 font-medium text-neutral-800 dark:text-neutral-200 relative z-20">
-        SplitIt is the best way to split expenses with your friends
-      </p>
-      <Button onClick={() => navigate("/splitit")} className="mt-4 z-10 dark:bg-white dark:text-black dark:hover:bg-white dark:hover:text-black dark:hover:shadow-none dark:hover:ring-0 cursor-pointer">Get Started</Button>
-    </div>
-    <HoverEffect className="bg-gradient-to-b from-slate-500 to-slate-700 dark:from-slate-700 dark:to-slate-900"
+      <div className="min-h-screen relative w-full overflow-hidden dark:bg-slate-900 flex flex-col gap-10 items-center justify-center">
+        <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
+        <Boxes />
+        <h1 className={cn("md:text-4xl text-xl font-semibold dark:text-white text-black relative z-20")}>
+          <PointerHighlight>SPLIT EXPENSES WITH FRIENDS</PointerHighlight>
+        </h1>
+        <p className="text-center mt-2 font-medium text-neutral-800 dark:text-neutral-200 relative z-20">
+          SplitIt is the best way to split expenses with your friends
+        </p>
+        <Button
+          onClick={handleGetStarted}
+          className="mt-4 z-10 dark:bg-white dark:text-black dark:hover:bg-white dark:hover:text-black dark:hover:shadow-none dark:hover:ring-0 cursor-pointer"
+        >
+          Get Started
+        </Button>
+      </div>
+      <HoverEffect className="bg-gradient-to-b from-slate-500 to-slate-700 dark:from-slate-700 dark:to-slate-900"
         items={[
           {
             title: "Multiple Spaces",
