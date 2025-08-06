1753bb4825a9ec136e8a65f744d7d5a8491c3805
Tarun Sharma <tarunsharma08.com@gmail.com>
2025-08-06 14:37:22 +0530

    socketIO integration

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import { createServer } from "http";
+import { Server } from "socket.io";
 
 import app from "./app.js";
 import connectToDb from "./db/db.js";
@@ -6,6 +7,31 @@ import connectToDb from "./db/db.js";
 const server = createServer(app);
 
 const PORT = process.env.PORT || 5000;
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  },
+});
+
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("join-space", (spaceId) => {
+    socket.join(spaceId);
+    console.log(`User joined space: ${spaceId}`);
+  })
+
+  socket.on("send-message", ({spaceId, message}) => {
+    io.to(spaceId).emit("receive-message", message)
+  })
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected")
+  })
+});
 
 connectToDb()
   .then(() => {
